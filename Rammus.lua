cc11b8885758ff4e9927269b24e909ddac073c1f
Teecolz <0kb240@gmail.com>
2014-07-18 10:27:33 -0700

    Update Rammus.lua

--- Rammus.lua
+++ Rammus.lua
@@ -3,19 +3,113 @@ if myHero.charName ~= "Rammus" then return end
 
 require 'SOW'
 require 'VPrediction'
+require 'Prodiction'
 
+--[AUTOUPDATER]--
+
+local version = "1.12"
+local author = "Teecolz"
+local scriptName = "Rammus"
+local AUTOUPDATE = false
+local UPDATE_HOST = "raw.github.com"
+local UPDATE_PATH = "/Teecolz/Teecolz---idylkarthus-Scripts/blob/master/Maokai.lua".."?rand="..math.random(1,10000)
+local UPDATE_FILE_PATH = SCRIPT_PATH.."Maokai.lua"
+local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
+
+DashList = {
+       ['Ahri']        = {true, spell = 'AhriTumble'},
+        ['Aatrox']      = {true, spell = 'AatroxQ'},
+        ['Akali']       = {true, spell = 'AkaliShadowDance'}, -- Targeted ability
+        ['Alistar']     = {true, spell = 'Headbutt'}, -- Targeted ability
+        ['Diana']       = {true, spell = 'DianaTeleport'}, -- Targeted ability
+        ['Gragas']      = {true, spell = 'GragasE'},
+        ['Graves']      = {true, spell = 'GravesMove'},
+        ['Hecarim']     = {true, spell = 'HecarimUlt'},
+        ['Irelia']      = {true, spell = 'IreliaGatotsu'}, -- Targeted ability
+        ['JarvanIV']    = {true, spell = 'jarvanAddition'}, -- Skillshot/Targeted ability
+        ['Jax']         = {true, spell = 'JaxLeapStrike'}, -- Targeted ability
+        ['Jayce']       = {true, spell = 'JayceToTheSkies'}, -- Targeted ability
+	['Katarina']	 = {true, spell = 'KatarinaE'},
+        ['Khazix']      = {true, spell = 'KhazixW'},
+        ['Leblanc']     = {true, spell = 'LeblancSlide'},
+        ['LeeSin']      = {true, spell = 'blindmonkqtwo'},
+        ['Leona']       = {true, spell = 'LeonaZenithBlade'},
+        ['Malphite']    = {true, spell = 'UFSlash'},
+        ['Maokai']      = {true, spell = 'MaokaiTrunkLine',}, -- Targeted ability	
+	['MasterYi']	=  {true, spell = 'AlphaStrike',}, -- Targeted
+        ['MonkeyKing']  = {true, spell = 'MonkeyKingNimbus'}, -- Targeted ability
+        ['Pantheon']    = {true, spell = 'PantheonW'}, -- Targeted ability
+        ['Pantheon']    = {true, spell = 'PantheonRJump'},
+        ['Pantheon']    = {true, spell = 'PantheonRFall' },
+        ['Poppy']       = {true, spell = 'PoppyHeroicCharge'}, -- Targeted ability
+        --['Quinn']       = {true, spell = 'QuinnE',                  range = 725,   projSpeed = 2000, }, -- Targeted ability
+        ['Renekton']    = {true, spell = 'RenektonSliceAndDice'},
+        ['Sejuani']     = {true, spell = 'SejuaniArcticAssault'},
+        ['Shen']        = {true, spell = 'ShenShadowDash'},
+        ['Tristana']    = {true, spell = 'RocketJump'},
+        ['Tryndamere']  = {true, spell = 'Slash'},
+        ['XinZhao']     = {true, spell = 'XenZhaoSweep'}, -- Targeted ability
+}
+StunList = {
+                { charName = "Katarina",        spellName = "KatarinaR" ,                  important = 0},
+                { charName = "Galio",           spellName = "GalioIdolOfDurand" ,          important = 0},
+                { charName = "FiddleSticks",    spellName = "Crowstorm" ,                  important = 1},
+                { charName = "FiddleSticks",    spellName = "DrainChannel" ,               important = 1},
+                { charName = "Nunu",            spellName = "AbsoluteZero" ,               important = 0},
+                { charName = "Shen",            spellName = "ShenStandUnited" ,            important = 0},
+                { charName = "Urgot",           spellName = "UrgotSwap2" ,                 important = 0},
+                { charName = "Malzahar",        spellName = "AlZaharNetherGrasp" ,         important = 0},
+                { charName = "Karthus",         spellName = "FallenOne" ,                  important = 0},
+                { charName = "Pantheon",        spellName = "PantheonRJump" ,              important = 0},
+                { charName = "Pantheon",        spellName = "PantheonRFall",               important = 0},
+                { charName = "Varus",           spellName = "VarusQ" ,                     important = 1},
+                { charName = "Caitlyn",         spellName = "CaitlynAceintheHole" ,        important = 1},
+                { charName = "MissFortune",     spellName = "MissFortuneBulletTime" ,      important = 1},
+                { charName = "Warwick",         spellName = "InfiniteDuress" ,             important = 0}
+}
+
+
+
+
+function AutoupdaterMsg(msg) print("<font color='#5F9EA0'><b>[".. scriptName .."] </font><font color='#cffffffff'> "..msg..".</font>") end
+if AUTOUPDATE then
+  local ServerData = GetWebResult(UPDATE_HOST, "/Teecolz/Teecolz---idylkarthus-Scripts/blob/master/Maokai.version")
+  if ServerData then
+    ServerVersion = type(tonumber(ServerData)) == "number" and tonumber(ServerData) or nil
+    if ServerVersion then
+      if tonumber(version) < ServerVersion then
+        AutoupdaterMsg("New version available"..ServerVersion)
+        AutoupdaterMsg("Updating, please don't press F9")
+        DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3)
+      else
+        AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")
+      end
+    end
+  else
+    AutoupdaterMsg("Error downloading version info")
+  end
+end
+
+-------------------------------------------------
+-------------------------------------------------
+
+--[Other Stuff]--
 local menu
 local ts
-local levelSequence = {}
+local levelSequence = {3, 1, 2, 1, 2, 4, 1, 2, 1, 2, 4, 2, 1, 3, 3, 4, 3, 3}
 local target
+local enemyTable = {}
+local PowerBallOn = false
+
+local Prodict
+local ProdictE, ProdictQ
+
+
+local TMTSlot, RAHSlot = nil, nil
+local TMTREADY, RAHREADY = false, false
+--[Spell Data]--
 local Qready, Wready, Eready, Rready = false, false, false, false
 
---[Spell Range]--
-local AARange = 125
-local Qrange  = 
-local Wrange  = 
-local Erange  = 325
-local Rrange  = 300
 
 -------------------------------------------------
 -------------------------------------------------
@@ -25,62 +119,95 @@ function OnLoad()
   iSOW  = SOW(VP)
   Menu()
   Init()
-  PrintChat("<font color=\"#78CCDB\"><b>" ..">> Rammus has been loaded")
+  PrintChat("<font color=\"#78CCDB\"><b>" ..">> Maokai has been loaded")
   Loaded = true
+  Variables()
   JungVariables()
-  EnemyMinions = minionManager(MINION_ENEMY, 325, myHero, MINION_SORT_HEALTH_ASC)
-end
+  EnemyMinions = minionManager(MINION_ENEMY, Erange, myHero, MINION_SORT_HEALTH_ASC)
   
+  for i, enemy in pairs(GetEnemyHeroes()) do
+    if enemy then 
+      local a = {}
+      a.object = enemy
+      a.stack = 0
+      table.insert(enemyTable, a)
+    end
+  end
+end
+
+    if VIP_USER then
+        Prodict = ProdictManager.GetInstance()
+        ProdictQ = Prodict:AddProdictionObject(_Q, Qrange, Qspeed, Qdelay, Qwidth)
+        ProdictE = Prodict:AddProdictionObject(_E, Erange, Espeed, Edelay, Ewidth)
+    end
+
+function Variables()
+
+  AARange = 125
+
+  Qrange = 600
+  Qwidth = 110 
+  Qspeed = 1200
+  Qdelay = 0.5
+
+  Wrange = 525
+  Wwidth = nil 
+  Wspeed = nil
+  Wdelay = 0
+
+  Erange = 325
+  Ewidth = 250 
+  Espeed = 1500 
+  Edelay = 1.0
+
+  Rrange = 625
+  Rwidth = 475 
+  Rspeed = nil 
+  Rdelay = 0.5
+end
+
+
 function Init()
   --[TargetSelector]--
-    ts      = TargetSelector(TARGET_LESS_CAST, 325, DAMAGE_PHYSICAL)
-    ts.name = "Rammus"
-end
-
+    ts      = TargetSelector(TARGET_LESS_CAST, 1100, DAMAGE_MAGIC)
+    ts.name = "Maokai"
+  end
+  
 function Menu()  
-        menu = scriptConfig("Rammus: Main Menu", "Rammus")
+        menu = scriptConfig("Maokai: Main Menu", "Maokai")
 
-          menu:addSubMenu("Rammus: Orbwalk", "Orbwalk")
+          menu:addSubMenu("Maokai: Orbwalk", "Orbwalk")
             iSOW:LoadToMenu(menu.Orbwalk)  
  
-          menu:addSubMenu("Rammus: Combo", "combo")
+          menu:addSubMenu("Maokai: Combo", "combo")
             menu.combo:addParam("combokey", "Combo",    SCRIPT_PARAM_ONKEYDOWN, false, 32)
-            menu.combo:addParam("useQ", "Use Q-Spell",  SCRIPT_PARAM_ONOFF, true)
-            menu.combo:addParam("useW", "Use W-Spell",  SCRIPT_PARAM_ONOFF, true)
-            menu.combo:addParam("useE", "Use E-Spell",  SCRIPT_PARAM_ONOFF, true)
-            menu.combo:addParam("useR", "Use R-Spell",  SCRIPT_PARAM_ONOFF, true)
-            menu.combo:addParam("useITEM", "Use Items",  SCRIPT_PARAM_ONOFF, true)
 
-          menu:addSubMenu("Rammus: Harass", "harass")
+
+          menu:addSubMenu("Maokai: Harass", "harass")
             menu.harass:addParam("harasskey", "Harass", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("C"))
-            menu.harass:addParam("useQ", "Use Q-Spell", SCRIPT_PARAM_ONOFF, true)
-            menu.harass:addParam("mana", "Dont Harass if mana < %", SCRIPT_PARAM_SLICE, 0, 0, 100)
+
             
-          menu:addSubMenu("Rammus: Lane Clear", "lane")
+          menu:addSubMenu("Maokai: Lane Clear", "lane")
             menu.lane:addParam("lanekey", "Lane Clear", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("Z"))
-            menu.lane:addParam("useQ", "Use Q-Spell", SCRIPT_PARAM_ONOFF, true)
-            menu.lane:addParam("useW", "Use W-Spell", SCRIPT_PARAM_ONOFF, true)
-            menu.lane:addParam("useITEM", "Use Items",  SCRIPT_PARAM_ONOFF, true)
+
             
-          menu:addSubMenu("Rammus: Jungle Clear", "jungle")
+          menu:addSubMenu("Maokai: Jungle Clear", "jungle")
             menu.jungle:addParam("junglekey", "Jungle Clear", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("Z"))
-            menu.jungle:addParam("useQ", "Use Q-Spell", SCRIPT_PARAM_ONOFF, true)
-            menu.jungle:addParam("useW", "Use W-Spell", SCRIPT_PARAM_ONOFF, true)
-            menu.jungle:addParam("useITEM", "Use Items",  SCRIPT_PARAM_ONOFF, true)
 
-          menu:addSubMenu("Rammus: Killsteal", "killsteal")
-            menu.killsteal:addParam("killstealR", "Use R-Spell to Killsteal", SCRIPT_PARAM_ONOFF, true)
-            menu.killsteal:addParam("killstealQ", "Use Q-Spell to Killsteal", SCRIPT_PARAM_ONOFF, true)
+          menu:addSubMenu("Maokai: Killsteal", "killsteal")
 
-          menu:addSubMenu("Rammus: Drawings", "draw")
-            menu.draw:addParam("drawAA", "Draw AA Range", SCRIPT_PARAM_ONOFF, true)
-            menu.draw:addParam("drawQ", "Draw Q Range",   SCRIPT_PARAM_ONOFF, true)
-            menu.draw:addParam("drawE", "Draw E Range",   SCRIPT_PARAM_ONOFF, true)
-            menu.draw:addParam("drawR", "Draw R Range",   SCRIPT_PARAM_ONOFF, true)
 
-          menu:addSubMenu("Rammus: Extras", "extra")
+          menu:addSubMenu("Maokai: Drawings", "draw")
+
+
+          menu:addSubMenu("Maokai: Extras", "extra")
             menu.extra:addParam("autolevel", "AutoLevel Spells", SCRIPT_PARAM_ONOFF, false)
+            menu.extra:addParam("prediction", "Prodiction = ON, VPred = OFF", SCRIPT_PARAM_ONOFF, true)
+            menu.extra:addParam("autoE", "Auto E under Turrets", SCRIPT_PARAM_ONOFF, false)
             menu.extra:addParam("debug", "Debug", SCRIPT_PARAM_ONOFF, false)
+	    menu.extra:addParam("QMethod", "Avoiding Method (False - Old, True - New)", SCRIPT_PARAM_ONOFF, true)
+	    menu.extra:addParam("QBuffer", "Distance to Avoid Minions",SCRIPT_PARAM_SLICE, 250, 0, 500, 1)
+            menu.extra:addParam("QBufferMove", "Distance to Move Away from minions",SCRIPT_PARAM_SLICE, 150, 0, 500, 1)
 
       --[PermaShow]--
       menu.combo:permaShow("combokey")
@@ -96,11 +223,14 @@ function OnTick()
     ts:update()
   end
   EnemyMinions:update()
+  UpdateAreas()
   spell_check()
   if menu.extra.autolevel then
     autoLevelSetSequence(levelSequence)
   end
+
   iSOW:EnableAttacks()
+  target = ts.target
 
   if menu.combo.combokey then
     Combo()
@@ -114,56 +244,102 @@ function OnTick()
   if menu.jungle.junglekey then
     JungleClear()
   end
-  TurretStun()
+  if menu.killsteal.killstealQ or menu.killsteal.killstealE then
+    killsteal()
+  end
+  if menu.extra.autoE then
+    TurretStun()
+  end
+
 end
 
 function spell_check()
   
-  TMTSlot, RAHSlot = GetInventorySlotItem(3077), GetInventorySlotItem(3074)
-  
-  TMTREADY = (TMTSlot ~= nil and myHero:CanUseSpell(TMTSlot) == READY)
-  RAHREADY = (RAHSlot ~= nil and myHero:CanUseSpell(RAHSlot) == READY)
-
   Qready = (myHero:CanUseSpell(_Q) == READY)
   Wready = (myHero:CanUseSpell(_W) == READY)
   Eready = (myHero:CanUseSpell(_E) == READY)
   Rready = (myHero:CanUseSpell(_R) == READY)
+
+end
+
+
+
+-------------------------------------------------
+-------------------------------------------------
+--[Ball Stuff]--
+function OnGainBuff(unit, buff)
+  if unit and unit.charName == myHero.charName and buff.name == "PowerBall" then
+     PowerBallOn = true
+  end
+end
+
+function OnLoseBuff(unit, buff)
+  if unit and unit.charName == myHero.charName and buff.name == "PowerBall" then
+     PowerBallOn = false
+  end
+end
+
+function AvoidMinions(Area)
+	myPosV = Vector(myHero.x, myHero.z)
+	AreaV = Vector(Area.x, Area.z)
+	calc = GetDistance(AreaV, myPosV)
+        dodgex = Area.x + ((menu.extra.QBufferMove + Area.range + (myHero.ms/100)^2)/calc)*(myHero.x-Area.x)  
+        dodgez = Area.z + ((menu.extra.QBufferMove + Area.range +(myHero.ms/100)^2)/calc)*(myHero.z-Area.z) 
+	MoveV = AreaV+(myPosV - AreaV):normalized() * (Area.range + menu.extra.QBufferMove + (myHero.ms/100)^2)
+	if menu.extra.QMethod then
+		myHero:MoveTo(dodgex, dodgez)
+	else
+		myHero:MoveTo(MoveV.x, MoveV.y)
+	end
+end
+
+function UpdateAreas()
+	MinionsArea = {}	
+	GetMinionsArea()
+end
+
+function GetMinionsArea()
+	for _, enemy in pairs(EnemyMinions.objects) do
+		if ValidTarget(enemy) then
+			QBuffer = menu.extra.QBuffer
+			table.insert(MinionsArea, {x = enemy.x, y = enemy.y, z = enemy.z, range = QBuffer})
+		end
+	end
 end
 
 -------------------------------------------------
 -------------------------------------------------
 --[Combo]--
 function Combo()
-  local Enemies = GetEnemyHeroes
-  local target  = ts.target
+	if Qready and not PowerBallOn then
+
+	end
+	for _, Area in pairs(MinionsArea) do
+		if GetDistance(Area) < Area.range + (myHero.ms/100)^2 then
+			AvoidMinions(Area)
+		end
+	end
+end
+
+-------------------------------------------------
+-------------------------------------------------
+--[Harass]--
+function Harass()
 
 end
 
 -------------------------------------------------
 -------------------------------------------------
 --[LaneClear]--
-function LaneClear() --[[DIDNT CHANGE, REDO THIS]]--
-  if not GetJungleMob() then
-    for i, minion in pairs(EnemyMinions.objects) do
-  if minion and minion.valid and not minion.dead then
-        if menu.lane.useQ and GetDistanceSqr(minion) <= 180625 then CastSpell(_Q) end
-        if menu.lane.useITEM and TMTREADY and GetDistance(minion) < 275 then CastSpell(TMTSlot) end
-        if menu.lane.useITEM and RAHREADY and GetDistance(minion) < 275 then CastSpell(RAHSlot) end
-      end    
-    end
-  end
+function LaneClear()
+
 end
 
 -------------------------------------------------
 -------------------------------------------------
 --[JungleCLear]--
-function JungleClear() --[[DIDNT CHANGE, REDO THIS]]--
-    local JungleMob = GetJungleMob()
-    if JungleMob ~= nil then
-      if menu.jungle.useQ and GetDistanceSqr(JungleMob) <= 180625 then CastSpell(_Q) end
-      if menu.jungle.useITEM and TMTREADY and GetDistance(JungleMob) < 275 then CastSpell(TMTSlot) end
-      if menu.jungle.useITEM and RAHREADY and GetDistance(JungleMob) < 275 then CastSpell(RAHSlot) end
-    end
+function JungleClear()
+
 end
 
 function GetJungleMob()
@@ -260,7 +436,7 @@ end
 function UnitAtTower(unit,offset)
   for i, turret in pairs(GetTurrets()) do
     if turret ~= nil then
-      if turret.team ~= myHero.team then
+      if turret.team == myHero.team then
         if GetDistance(unit, turret) <= turret.range+offset then
           return true
         end
@@ -279,20 +455,18 @@ function TurretStun()
 end
 -------------------------------------------------
 -------------------------------------------------
+--[KillSteal]--
+function killsteal()
+
+end
+
+-------------------------------------------------
+-------------------------------------------------
 --[OnDraw]--
 function OnDraw()
   if myHero.dead then return end
-  draw_Range()
+  PowerDistance = (menu.extra.QBuffer + menu.extra.QBufferMove + (myHero.ms/100)^2)
+  DrawText3D("["..PowerDistance.."]", myHero.x, myHero.y, myHero.z, 20, RGB(0, 255, 0), 0)
 end
 
-function draw_Range()
-  if menu.draw.drawAA then
-    DrawCircle(myHero.x, myHero.y, myHero.z, AARange, ARGB(255,100,0,50))
-  end
-  if menu.draw.drawE and Eready then
-    DrawCircle(myHero.x, myHero.y, myHero.z, Erange, ARGB(255,100,0,50))
-  end
-  if menu.draw.drawR and Rready then
-    DrawCircle(myHero.x, myHero.y, myHero.z, Rrange, ARGB(255,100,0,50))
-  end
-end
+
